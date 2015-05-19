94dc3764172940b505479ddaf315acfa5af84be4
André Bezerra <andreqbs@MacBook-Pro.local>
2015-05-19 14:59:59 -0300

    Alterações

--- src/unp/br/pilha/InterfacePilhaEncadeada.java
+++ src/unp/br/pilha/InterfacePilhaEncadeada.java
@@ -9,14 +9,13 @@ package unp.br.pilha;
  *
  * @author andreqbs
  */
-public interface interfacePilhaArranjo {
-    
-    void criarPilha(int tamanho);
+public interface InterfacePilhaEncadeada {
+    void criarPilha();
     boolean eVazia (); //verificar se está vazia a pilha
-    boolean eCheia (); //verificar se está vazia a pilha
     int tamanho(); //retorna o número de itens da pilha 
-    No push(int valor); //insere um valor no topo da pilha
-    No pop(); //retira o valor no topo da pilha
+    void limpar(); //esvaziar pilha
+    NoEncadeado push(int valor); //insere um valor no topo da pilha
+    NoEncadeado pop(); //retira o valor no topo da pilha
     int pegarTopo(); //retorna o valor atual do topo da pilha
-     
+    void listar(); //retorna todos os valores da pilha
 }
